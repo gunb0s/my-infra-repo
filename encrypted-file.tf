b42ebb415a9ad7b5c7d8fda8a99aaeaa56dd9b6f
gunb0s <gubos1031@gmail.com>
2025-05-07 01:15:12 +0900

    .

--- encrypted-file.tf
+++ encrypted-file.tf
@@ -2,25 +2,25 @@ provider "aws" {
     region = "ap-northeast-2"
 }
 
-# data "aws_caller_identity" "self" {}
+data "aws_caller_identity" "self" {}
 
-# data "aws_iam_policy_document" "cmk_admin_policy" {
-#     statement {
-#         effect = "Allow"
-#         resources = ["*"]
-#         actions = ["kms:*"]
-#         principals {
-#             type = "AWS"
-#             identifiers = [data.aws_caller_identity.self.arn]
-#         }
-#     }
-# }
+data "aws_iam_policy_document" "cmk_admin_policy" {
+    statement {
+        effect = "Allow"
+        resources = ["*"]
+        actions = ["kms:*"]
+        principals {
+            type = "AWS"
+            identifiers = [data.aws_caller_identity.self.arn]
+        }
+    }
+}
 
-# resource "aws_kms_key" "cmk" {
-#     policy = data.aws_iam_policy_document.cmk_admin_policy.json
-# }
+resource "aws_kms_key" "cmk" {
+    policy = data.aws_iam_policy_document.cmk_admin_policy.json
+}
 
-# resource "aws_kms_alias" "cmk" {
-#     name = "alias/kms-cmk-example"
-#     target_key_id = aws_kms_key.cmk.id
-# }
+resource "aws_kms_alias" "cmk" {
+    name = "alias/kms-cmk-example"
+    target_key_id = aws_kms_key.cmk.id
+}
